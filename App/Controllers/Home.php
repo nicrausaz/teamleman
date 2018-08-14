35f63129023ae7689da70d513aa07d43ba005829
nicrausaz <n.crausaz99@gmail.com>
2018-08-14 21:48:12 +0200

    BASE INDEX

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -4,21 +4,11 @@ namespace App\Controllers;
 
 use \Core\View;
 
-/**
- * Home controller
- *
- * PHP version 7.0
- */
 class Home extends \Core\Controller
 {
-
-    /**
-     * Show the index page
-     *
-     * @return void
-     */
     public function indexAction()
     {
+        // $player = Models\Player->getAll();
         View::renderTemplate('Home/index.html');
     }
 }
