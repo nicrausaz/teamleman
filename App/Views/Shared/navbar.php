fd19f2579fca3be505457387f51505d2708e7a87
nicrausaz <n.crausaz99@gmail.com>
2018-08-14 21:48:12 +0200

    BASE INDEX

---
+++ App/Views/Shared/navbar.php
@@ -0,0 +1,13 @@
+<header class="navbar navbar-expand flex-column flex-md-row bd-navbar fixed-top">
+  <a class="navbar-brand" href="/">
+    <img src="App/assets/imgs/logo3.jpg" width="150" height="130">
+  </a>
+  <nav class="nav nav-masthead justify-content-center navbar-expand-lg">
+    <a class="nav-link active" href="#about">Présentation</a>
+    <a class="nav-link" href="#players">Joueurs</a>
+    <a class="nav-link" href="#sponsors">Sponsors</a>
+    <a class="nav-link" href="#results">Résultats</a>
+    <a class="nav-link" href="#coming">A venir</a>
+    <a class="nav-link" href="#media">Médias</a>
+  </nav>
+</header>
