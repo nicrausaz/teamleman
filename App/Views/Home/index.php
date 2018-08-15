afe82e0a8dcecd3782d31bcfc3482944c434b238
nicrausaz <n.crausaz99@gmail.com>
2018-08-15 17:02:07 +0200

    textes

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -19,35 +19,35 @@
   <div class="part">
     <a class="anchor" id="players"></a>
     <div class="container">
-      <h1 class="display-4">Fluid jumbotron</h1>
+      <h1 class="display-4">Joueurs</h1>
       <p class="lead"><?php print_r($players) ?></p>
     </div>
   </div>
   <div class="part">
     <a class="anchor" id="sponsors"></a>
     <div class="container">
-      <h1 class="display-4">Fluid jumbotron</h1>
+      <h1 class="display-4">Sponsors</h1>
       <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
   </div>
   <div class="part">
     <div class="container">
       <a class="anchor" id="results"></a>
-      <h1 class="display-4">Fluid jumbotron</h1>
+      <h1 class="display-4">Résultats</h1>
       <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
   </div>
   <div class="part">
     <div class="container">
       <a class="anchor" id="coming"></a>
-      <h1 class="display-4">Fluid jumbotron</h1>
+      <h1 class="display-4">A venir</h1>
       <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
   </div>
   <div class="part ">
     <div class="container">
       <a class="anchor" id="media"></a>
-      <h1 class="display-4">Fluid jumbotron</h1>
+      <h1 class="display-4">Médias</h1>
       <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
   </div>
