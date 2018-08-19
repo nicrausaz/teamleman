9d7388b761212bde920d4230e09cb39be263de76
nicrausaz <n.crausaz99@gmail.com>
2018-08-19 17:46:49 +0200

    rebase, get players

---
+++ Controllers/Players.php
@@ -0,0 +1,16 @@
+<?php
+  require "./DB/DBContext.php";
+  class Players {
+    public $db;
+
+    function __construct () {
+      $this->db = new DBContext();
+    }
+
+    public function getAll () {
+      $this->db->connect();
+      $req = $this->db->pdo->prepare('SELECT * FROM T_Players');
+      $req->execute();
+      return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+  }
---
+++ Templates/head.php
@@ -0,0 +1,22 @@
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-124188532-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-124188532-1');
+  </script>
+  <link rel="icon" href="assets/imgs/favicon.png" />
+  <title>Team Leman 3x3</title>
+  <?php
+    require "./Controllers/Players.php";
+    $players = new Players();
+  ?>
+
+</head>
