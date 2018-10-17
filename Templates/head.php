1385aca05f639d580b387fde7f8a8034a0def703
nicrausaz <n.crausaz99@gmail.com>
2018-10-17 08:36:01 +0200

    a bit responsive

--- Templates/head.php
+++ Templates/head.php
@@ -1,5 +1,6 @@
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.12/css/uikit.min.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.12/js/uikit.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.12/js/uikit-icons.min.js"></script>
