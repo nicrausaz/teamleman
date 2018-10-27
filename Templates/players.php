5a3d9ddf9e76afda9ce6b512ea2330d347ce8f85
nicrausaz <n.crausaz99@gmail.com>
2018-10-27 16:20:40 +0200

    insta post, card palyers

--- Templates/players.php
+++ Templates/players.php
@@ -1,13 +1,13 @@
 <div class="uk-section uk-dark" id="players" uk-scrollspy="cls:uk-animation-fade">
   <h1 class="uk-text-right">Joueurs</h1>
-  <div class="uk-child-width-1-5@m" uk-grid uk-scrollspy="cls: uk-animation-slide-left">
+  <div class="uk-child-width-1-4@m" uk-grid uk-scrollspy="cls: uk-animation-slide-left">
   <?php foreach ($players->getAll() as $player) { ?>
     <div>
       <div class="uk-card uk-card-default">
         <div class="uk-card-media-top">
         <?php
         if ($player['player_pic'] != "") { ?>
-          <img src="assets/imgs/players/<?= $player['player_pic'] ?>" alt="playerpic">
+          <img src="assets/imgs/players/<?= $player['player_pic'] ?>" alt="playerpic" style="height: 400px; width: 100%;">
         <?php } else { ?>
           <img src="assets/imgs/nopicture.jpg" alt="playerpic">
         <?php } ?>
